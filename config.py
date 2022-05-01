dc5eed04839010f98d60a7decd18eebe763467d6
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-05-01 14:06:36 -0700

    Revert "Need the linuxbsd build to work so I can export V-Sekai."
    This reverts commit 390a792f142948ab0476f68c29ce3c50a4d95e17.

--- config.py
+++ config.py
@@ -3,8 +3,8 @@
 
 def can_build(env, platform):
     # Not presently available on web
-    return platform != "javascript" and platform != "linuxbsd"
-  
+    return platform != "javascript"
+
 
 def configure(env):
     pass
